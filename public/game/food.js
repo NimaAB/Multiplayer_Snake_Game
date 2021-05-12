d2e22d27495539940a7fec2cbc4e1f24624d4f0f
NimaAB <nimabewrani98@gmail.com>
2021-05-13 00:40:21 +0200

    det ligger en bug i eat()

--- public/game/food.js
+++ public/game/food.js
@@ -1,8 +1,8 @@
 export default class Food{
     constructor(game) {
         this.posision = {
-            x: Math.floor(Math.random()*game.width),
-            y: Math.floor(Math.random()*game.height)
+            x: Math.floor((Math.random()*game.width - 50) + 50),
+            y: Math.floor((Math.random()*game.height - 50) + 50)
         };
         this.UNIT = game.UNIT;
         //this.food_image = document.getElementById("food")
