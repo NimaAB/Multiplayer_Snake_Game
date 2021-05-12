112534f90bfd3bc57bad3bdb48041ab741c60f72
NimaAB <nimabewrani98@gmail.com>
2021-05-12 13:20:46 +0200

    added collision dectection for the food

--- public/game/collisionDetection.js
+++ public/game/collisionDetection.js
@@ -1,31 +1,30 @@
-export default class CollisionDetection {
-    constructor(game, snake) {
-        this.game = game;
-        this.snake = snake;
-    }
+export function wallCollisions(game, snake) {
+    let head = snake.posisions[snake.posisions.length - 1];
+    let head_x_pos = head[0];
+    let head_y_pos = head[1];
 
-    wallCollisions() {
-        //Not working properly yet
-        let head = this.snake.posisions[this.snake.posisions.length - 1];
-        let head_x_pos = head[0];
-        let head_y_pos = head[1];
+    return head_x_pos === 0
+        || head_x_pos === snake.SPEED
+        || head_x_pos === -snake.SPEED
+        || head_x_pos === game.width - snake.SPEED
+        || head_x_pos === game.width + snake.SPEED
+        // up & down walls:
+        || head_y_pos === 0
+        || head_y_pos === snake.SPEED
+        || head_y_pos === -snake.SPEED
+        || head_y_pos === game.height - snake.SPEED
+        || head_y_pos === game.height + snake.SPEED;
+}
 
-        if (
-            //left & right walls:
-            head_x_pos === 0
-            || head_x_pos === this.snake.SPEED
-            || head_x_pos === -this.snake.SPEED
-            || head_x_pos === this.game.width - this.snake.SPEED
-            || head_x_pos === this.game.width + this.snake.SPEED
-            // up & down walls:
-            || head_y_pos === 0
-            || head_y_pos === this.snake.SPEED
-            || head_y_pos === -this.snake.SPEED
-            || head_y_pos === this.game.height - this.snake.SPEED
-            || head_y_pos === this.game.height + this.snake.SPEED
-        ) {
-            this.game.STATE = "DEAD";
-        }
-
-    }
+export function foodEaten(snake, food) {
+    let head = snake.posisions[snake.posisions.length - 1];
+    let head_x_pos = head[0];
+    let head_y_pos = head[1];
+    let food_x_pos = food.posision.x;
+    let food_y_pos = food.posision.y;
+    return (head_x_pos === food_x_pos && head_y_pos === food_y_pos)
+        || (head_x_pos === food_x_pos + 2 && head_y_pos === food_y_pos)
+        || (head_x_pos === food_x_pos - 2 && head_y_pos === food_y_pos)
+        || (head_x_pos === food_x_pos && head_y_pos === food_y_pos + 2)
+        || (head_x_pos === food_x_pos && head_y_pos === food_y_pos - 2);
 }
