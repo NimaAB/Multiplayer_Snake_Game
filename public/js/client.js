956c53538807ec82844e0f03ab8768b6a78fdc80
Glaysa <glaysa.df@gmail.com>
2021-05-18 14:44:47 +0200

    removed waiting players alert and its socket event

--- public/js/client.js
+++ public/js/client.js
@@ -1,17 +1,15 @@
 import { game_initializer, drawGame } from './index.js';
 
 const gameOverElement = document.getElementById("gameOver");
-const waitingForPlayers = document.getElementById("waitingForPlayers");
 const tooManyPlayers = document.getElementById("tooManyPlayers");
 
 const socket = io(`${location.protocol}//${document.domain}:${location.port}`);
 
 socket.on('init', () => {
     game_initializer();
-})
+});
 
 socket.on('new_game_state', (gameState) => {
-    waitingForPlayers.style.display = 'none';
     requestAnimationFrame(()=>{
         drawGame( JSON.parse(gameState));
     });
@@ -24,10 +22,6 @@ socket.on('game_over',(player)=>{ //Her kan vi for eksempel få total poenger fr
     p_tag.classList.add("order-2");
     gameOverElement.appendChild(p_tag);
     gameOverElement.style.display = "flex";
-});
-
-socket.on('waiting_for_players', () => {
-    waitingForPlayers.style.display = 'flex';
 });
 
 socket.on('too_many_players', () => {
